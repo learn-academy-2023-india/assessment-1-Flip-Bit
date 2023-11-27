510b5e4ddfb0fb76e46aa89c5073e115971665e7
Biter <flipbit@mailfence.com>
2023-11-27 09:16:27 -0800

    complete

--- coding-concepts.js
+++ coding-concepts.js
@@ -11,7 +11,9 @@ const colors = ["tangerine", "magenta", "lilac", "daffodil"]
 // console.log(colors.push("indigo"))
 
 // a) Your answer:
+//Will log the array with indigo at the last index.
 // b) Verify and explain:
+//if you run the console.log itll give 5, meaning something was added
 
 // --------------------1) What will this log?
 
@@ -19,7 +21,9 @@ const cohort = "LEARN 2023"
 // console.log(cohort.length)
 
 // a) Your answer:
+//It will count the characters within the cohort variable
 // b) Verify and explain:
+//I was wrong, it just said the number of characters
 
 // --------------------2) What will this log?
 
@@ -27,16 +31,20 @@ const greeting = "Hello World!"
 // console.log(greeting[4])
 
 // a) Your answer:
+//Will show the fourth character
 // b) Verify and explain:
+//I was wrong, it said "o"
 
 // --------------------3) What will this log?
 
 const languages = ["JavaScript", "Ruby", "Python", "C++"]
-const index = 1
+// const index = 1
 // console.log(languages[index])
 
 // a) Your answer:
+//Will show Ruby, JS should be 0
 // b) Verify and explain:
+//Verified, correct
 
 // --------------------4) What will this log?
 
@@ -44,12 +52,16 @@ const weekendDays = ["saturday", "sunday"]
 // console.log(weekendDays.toUpperCase())
 
 // a) Your answer:
+//Give an error, I think because it is an array and not a string
 // b) Verify and explain:
+//It did give an error, not sure if Im 100% on the why
 
 // --------------------5) What will this log?
 
 const dataTypes = ["number", "string", "Boolean", "undefined"]
-// console.log(typeof dataTypes.length)
+console.log(typeof dataTypes.length)
 
 // a) Your answer:
+//Sjiuld give the length of the datatype
 // b) Verify and explain:
+//Incorrect, showed numbers
